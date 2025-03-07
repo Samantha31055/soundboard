e1596ad6646189cdfbc6b75051881b99957dc2b1
samantha avalos <samantha3105523@gmail.com>
2025-03-06 19:03:22 -0500

    Add mixkit-tick-tock-clock-close-up-1059.wav to souds/

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ sounds.forEach((sound) => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = sound;
+    btn.addEventListener('click', () => {
+        stopSounds();
+        document.getElementById(sound).play();
+    });
     document.getElementById('buttons').appendChild(btn);
 });
+
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const snd = document.getElementById(sound);
+
+        snd.pause();
+        snd.currentTime = 0;
+    });
+}
